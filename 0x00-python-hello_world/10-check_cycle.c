8f13e96299b167fea4fee68ed13df3a22fce50fd
imperfectskillz <j.choi.89@gmail.com>
2017-12-07 00:36:51 +0000

    completed tortoise and hare

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,5 +8,22 @@
 
 int check_cycle(listint_t *list)
 {
+	//same struct for both fast and slow
+	listint_t *slow;
+	listint_t *fast;
 
+	if (list == NULL)
+		return (0);
+
+	//check if fast's next and next are NULL
+	while (fast->next != NULL && fast->next->next != NULL)
+	{
+		//move slow by one
+		slow = slow->next;
+		//move fast by two
+		flast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
 }
