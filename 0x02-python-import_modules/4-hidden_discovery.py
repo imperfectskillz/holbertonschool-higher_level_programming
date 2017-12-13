91bc9607611dc1b84b99d27aae8aaae9a2d1bd36
imperfectskillz <j.choi.89@gmail.com>
2017-12-13 21:33:32 +0000

    checking first byte

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import hidden_4
     hidden = dir(hidden_4)
     for i in range(len(hidden)):
-        if hidden[i][0:1] != "_":
+        if hidden[i][0] != "_":
             print("{}".format(hidden[i]))
