102b45d8bd228cfaf318d171cc7af651790ad8b7
imperfectskillz <j.choi.89@gmail.com>
2017-12-13 21:32:38 +0000

    added range

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     import hidden_4
     hidden = dir(hidden_4)
-    for i in len(hidden):
+    for i in range(len(hidden)):
         if hidden[i][0:1] != "_":
             print("{}".format(hidden[i]))
