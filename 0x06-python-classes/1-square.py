9ac93985981aef6ba3218f1da595a8e3550f6afc
imperfectskillz <j.choi.89@gmail.com>
2018-01-04 07:38:06 +0000

    so confused

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """This is a class Square with private instance attr. size"""
+
+    def __init__(self, size):
+        """instantiate with no value or type"""
+        self.__size = size
