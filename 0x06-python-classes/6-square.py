c8008a397d0e6411ac85eb207f041de426ca40f9
imperfectskillz <j.choi.89@gmail.com>
2018-01-07 08:51:39 +0000

    pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 class Square:
     """class Square defined"""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """initializes size"""
         self.__size = size
         self.__position = position
