27a3a3b41a015fcab5edd65d47d7c7bcc4d04ae3
imperfectskillz <j.choi.89@gmail.com>
2018-01-08 07:35:59 +0000

    added more cases to unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,4 +7,13 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    
+    def test_true_int(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([1, 2]), 2)
+
+    def test_negative_int(self):
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([-1, -2, 33]), 33)
+
+    def test_string(self):
+        self.assertEqual(max_integer(["1", "2"]), "2")
