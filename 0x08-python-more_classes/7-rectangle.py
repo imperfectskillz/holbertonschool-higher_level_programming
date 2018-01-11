53ec1fd965a0710e35053f0f86809985d5aa81be
imperfectskillz <j.choi.89@gmail.com>
2018-01-11 23:59:24 +0000

    spelling error: print_symbol fixed

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     includes attributes for this class
     """
     number_of_instances = 0
-    print_symbole = "#"
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """initializes width and height"""
