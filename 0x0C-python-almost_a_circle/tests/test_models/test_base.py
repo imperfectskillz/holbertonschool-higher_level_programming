59cd3952131cb39144c308c90c685f6e5b2f132d
imperfectskillz <j.choi.89@gmail.com>
2018-01-24 07:35:31 +0000

    syntax

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Unittest class Base
-"""
+"""Unittest class Base"""
 import unittest
 from models.base import Base
 
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Unittest for Rectangle
+"""
+import json
+import unnittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    
