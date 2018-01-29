02cf33da3a938d3e1d6c211ff56ce43dbeecd1e8
imperfectskillz <j.choi.89@gmail.com>
2018-01-29 08:41:21 +0000

    spelling error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except exception as error:
+    except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
