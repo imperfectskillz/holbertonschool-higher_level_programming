3ac1f617b7d69c5ebd4b450a1122a4e6e5eceb3a
imperfectskillz <j.choi.89@gmail.com>
2018-03-01 23:22:09 +0000

    changed id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn
-SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
