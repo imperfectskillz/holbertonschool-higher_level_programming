a7589610c8ee6b25b1b8e1713107312448c24b8c
imperfectskillz <j.choi.89@gmail.com>
2018-03-01 23:16:30 +0000

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genres from hbtn
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC;
