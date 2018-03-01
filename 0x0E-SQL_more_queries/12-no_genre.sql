b3d1cf8ac539764784d40a4745d8da9aecefef03
imperfectskillz <j.choi.89@gmail.com>
2018-03-01 23:22:49 +0000

    inner join

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
