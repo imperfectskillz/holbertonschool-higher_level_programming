d047e87d4ccff6be464bde1d59645b378cd1a313
imperfectskillz <j.choi.89@gmail.com>
2018-03-02 22:23:50 +0000

    complete

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- List same score numbers
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- List records with name value
+SELECT score, name FROM second_table WHERE NAME IS NOT NULL ORDER BY score DESC;
