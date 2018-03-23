703d63457417a5c10d958f793cb8d5f637c94658
imperfectskillz <j.choi.89@gmail.com>
2018-03-23 13:05:52 +0000

    uh oh

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script listing all states with name starting with N
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    curse = db.cursor()
+    curse.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id WHERE states.name = %s ORDER BY cities.id ASC", (argv[4],))
+    for row in curse.fetchall():
+        print(row)
+    curse.close()
+    db.close()
