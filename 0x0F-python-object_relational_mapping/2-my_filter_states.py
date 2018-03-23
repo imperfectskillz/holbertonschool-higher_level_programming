dec206ccb96bd5f173cd09ed658b5aade52e8f0e
imperfectskillz <j.choi.89@gmail.com>
2018-03-23 11:06:01 +0000

    initial

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script listing all states with name starting with N
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    curse = db.cursor()
+    curse.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+    for row in curse.fetchall():
+        print(row)
+    curse.close()
+    db.close()
