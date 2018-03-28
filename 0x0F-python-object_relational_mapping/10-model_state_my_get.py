093397db0d70b5f1a7297888fe03363cfc1dd801
imperfectskillz <j.choi.89@gmail.com>
2018-03-28 22:06:31 +0000

    10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Uses SQLAlchemy to print state objects
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    result = session.query(State).filter(State.name == argv[4]).order_by(State.id).all()
+    if result:
+        print(result[0].id)
+    else:
+        print("Not found")
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     curse = db.cursor()
-    curse.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id WHERE states.name = %s ORDER BY cities.id ASC", (argv[4],))
+    curse.execute("SELECT cities.name FROM cities JOIN states ON states_id = cities.state_id WHERE states.name = %s ORDER BY cities.id ASC", (argv[4], ))
     for row in curse.fetchall():
         print(row)
     curse.close()
