08fafca0fc53dabe041bd81d278c6f822fabec1e
imperfectskillz <j.choi.89@gmail.com>
2018-04-04 01:04:20 +0000

    i will always be HERE

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request with variables
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be there for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
