de36abad193ed35d85775cbd3e53f33d15814fa9
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 12:02:54 +0000

    email spelling

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    payload = {'e-mail': argv[2]}
+    payload = {'email': argv[2]}
     r = requests.post(argv[1], data=payload)
     print(r.text)
