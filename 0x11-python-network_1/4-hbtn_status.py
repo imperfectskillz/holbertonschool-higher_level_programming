b98d6c1c277324d74d8833f34d5df2f777e1d735
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 11:37:36 +0000

    hardcoded url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text)))
