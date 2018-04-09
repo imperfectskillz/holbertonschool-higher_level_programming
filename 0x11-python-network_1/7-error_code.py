7efe93eeb16c2b839117a4bcc1b6426c7c863f59
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 12:31:22 +0000

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+error code return if greater than 400
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[2])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
