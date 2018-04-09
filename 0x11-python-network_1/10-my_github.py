5821362c08308c0857f335c9e2d45d70c781dd2a
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 14:55:54 +0000

    get

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     json = r.json()
     if json:
-        print(json['id'])
+        print(json.get['id'])
     else:
         print("None")
