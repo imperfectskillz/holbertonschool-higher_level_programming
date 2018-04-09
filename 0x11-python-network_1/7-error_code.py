4d5a38a0186c5e6c836257cb19703419e0f7eeb5
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 13:51:19 +0000

    argv1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get(argv[2])
+    r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
