4ba726e2f688143f76357c927433dbbaa5fe9d36
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 11:13:26 +0000

    spelling error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         answer = response.read()
-        print("Body responses:")
+        print("Body response:")
         print("\t- type: {}".format(type(answer)))
         print("\t- content: {}".format(answer))
         print("\t- utf8 content: {}".format(answer.decode("utf-8")))
