f1a062b16ca2f298011e18296bc8d893fdba5308
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 14:53:45 +0000

    syntax

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     json = r.json()
-    if json is not None:
+    if json:
         print(json['id'])
     else:
         print("None")
