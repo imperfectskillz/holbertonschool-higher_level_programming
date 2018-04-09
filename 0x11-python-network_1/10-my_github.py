7d7eaa7b9f58828cd407d20c8e262f8b4eb93f1d
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 15:20:05 +0000

    changed try and except

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     json = r.json()
-    if json:
-        print(json.get['id'])
-    else:
+    try:
+        print(json['id'])
+    except:
         print("None")
