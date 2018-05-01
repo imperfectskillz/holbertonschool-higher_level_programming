f110b8e506c9b577b2e6b073427f3c715f2d934c
imperfectskillz <j.choi.89@gmail.com>
2018-05-01 10:32:40 +0000

    took out var

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-var i = 0;
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurences');
 } else {
-  while (i < process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
-    i++;
   }
 }
