6b2032fbc413e6be46c36274697fc7746ac65d1b
imperfectskillz <j.choi.89@gmail.com>
2018-05-01 11:15:02 +0000

    trying again

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,6 @@
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  let num = parseInt(process.argv[2]);
+  console.log('My number: ' + num);
 }
