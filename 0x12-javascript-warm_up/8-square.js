2945a477e65399a39e3869c08260c23790be3ec9
imperfectskillz <j.choi.89@gmail.com>
2018-05-01 08:23:09 +0000

    omitted var

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-var i;
-var j;
-
 if (process.argv[2])
 {
-    for (i = 0; i < process.argv[2]; i++)
+    for (let i = 0; i < process.argv[2]; i++)
     {
 	console.log('X'.repeat(process.argv[2]));
     }
