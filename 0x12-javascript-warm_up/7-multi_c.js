1ddb73bf26eb66a2df55d4691f3f237b97b530e8
imperfectskillz <j.choi.89@gmail.com>
2018-05-01 10:30:24 +0000

    spelling error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 var i = 0;
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing number of occurences');
 } else {
   while (i < process.argv[2]) {
     console.log('C is fun');
