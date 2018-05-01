65ec46fbdddab8f2dee3611a4ad2020e4a451d33
imperfectskillz <j.choi.89@gmail.com>
2018-05-01 08:49:42 +0000

    almost done

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial(num) {
+    if (isNaN(num)) {
+	console.log(1);
+	}
+    else if (num == 0) {
+	return 1;
+	}
+    else {
+	return (num * factorial(num - 1));
+    }
+}
+console.log(factorial(process.argv[2]));
