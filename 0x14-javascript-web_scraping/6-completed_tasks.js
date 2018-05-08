d3777b1655d9a40befae83cd3d12d5447123ef1e
imperfectskillz <j.choi.89@gmail.com>
2018-05-08 06:52:55 +0000

    6 worksgit add .

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,23 @@
 #!/usr/bin/node
 
 let request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+    if (error) {
+	console.log(error);
+    } else {
+	let result = {};
+	let scraped = JSON.parse(body);
+	for (let each in scraped) {
+	    let current = scraped[each];
+	    let userid = current['userId'];
+	    if (current['completed'] === true &&
+		result[userid] === undefined) {
+		result[userid] = 1;
+	    } else if (current['completed'] === true) {
+		result[userid]++;
+	    }
+	}
+	console.log(result);
+    }
+});
